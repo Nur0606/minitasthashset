e17292d5d33918efa52d5eaf83433d813a195854
nurhan <141030168+Nur0606@users.noreply.github.com>
2023-12-12 19:54:32 +0600

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,88 @@
+import java.util.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+
+//        HashSet<Integer> hashSet = new HashSet<>();
+//        hashSet.add(1);
+//        hashSet.add(3);
+//        hashSet.add(6);
+//        System.out.println(hashSet);
+        //1
+//        for (int integer : hashSet) {
+//            System.out.println(integer);
+//////        }
+            //2
+//        int num  = hashSet.size();
+//        System.out.println("razmer: "+num);
+
+            //3
+//        hashSet.clear();
+
+        //4
+//        if (hashSet.isEmpty()){
+//            System.out.println("bosh");
+//        }else
+//            System.out.println("boshmec");
+//5
+//        HashSet<Integer>hashSet1 = new HashSet<>();
+//        hashSet1.add(3);
+//        hashSet1.add(99);
+//        hashSet1.add(38);
+//        System.out.println("do obmena: "+hashSet1);
+//   TreeSet<Integer > treeSet = new TreeSet<>(hashSet1);
+//        System.out.println("posle obmena: "+treeSet);
+        //6
+//        LinkedList<Integer > list = new LinkedList<>();
+//        list.add(6);
+//        list.add(99);
+//        list.add(1);
+//        list.add(10);
+//        System.out.println(": "+list);
+//        HashSet<Integer > hashSet1 = new HashSet<>(list);
+//        System.out.println(": "+hashSet1);
+        //7
+//        HashSet<Integer> hashSet1 = new HashSet<>();
+//        HashSet<Integer> hashSet2 = new HashSet<>();
+//             hashSet1.add(1);
+//             hashSet1.add(1);
+//             hashSet1.add(2);
+//             hashSet1.add(3);
+//             hashSet1.add(3);
+//             hashSet1.add(3);
+//             hashSet1.add(3);
+//             hashSet1.add(3);
+//             hashSet1.add(3);
+//             hashSet1.add(8);
+//
+//        hashSet2.add(4);
+//        hashSet2.add(4);
+//        hashSet2.add(5);
+//        hashSet2.add(6);
+//        hashSet2.add(2);
+//        hashSet2.add(6);
+//        hashSet2.add(7);
+//        hashSet2.add(7);
+//        hashSet2.add(9);
+//        hashSet1.addAll(hashSet2);
+//        for (int a:hashSet1) {
+//            System.out.println(a);
+//
+//        }
+//        System.out.println(hashSet1+""+hashSet2);
+//        HashSet<String> hashSet = new HashSet<>();
+//        hashSet.add("Java");
+//        hashSet.add("Python");
+//        hashSet.add("JavaScript");
+//        hashSet.add("git");
+//        String[] array = new String[hashSet.size()];
+//        hashSet.toArray(array);
+//        System.out.println(" : ");
+//        System.out.println(Arrays.toString(array));
+
+
+
+    }
+}
